5e1f1df3aa6e2e3ccb057307e062fdb22f0ff362
Miguel Ibars <miguelpi@ext.zara.com>
2022-04-03 19:30:17 +0200

    feat: Implement tabs

--- js/feature/my-links.js
+++ js/feature/my-links.js
@@ -1,11 +1,16 @@
 import { API } from "../api";
+import { currentUser } from "../auth";
 import { Ui } from "../ui";
 
 export const MyLinks = {
   init: () => {
-    API.list().then((urls) => urls.forEach(display));
+    currentUser().then(loadUserUrls).catch(showSimpleUi);
   },
 };
+
+function loadUserUrls() {
+  API.list().then((urls) => urls.forEach(display));
+}
 
 function display(url) {
   const newLinkItem = Ui.Lists.myLinks.querySelector("li").cloneNode(true);
@@ -14,4 +19,10 @@ function display(url) {
   a.textContent = url.shortened_url;
   newLinkItem.show();
   Ui.Lists.myLinks.appendChild(newLinkItem);
+}
+
+function showSimpleUi() {
+  const form = Ui.Forms.shorten;
+  const tabs = document.querySelector(".tabs");
+  tabs.replaceWith(form);
 }
